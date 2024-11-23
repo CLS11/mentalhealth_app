453257acb3cd6a33c1af287707dc91462809558b
CLS11 <crystall.101600@gmail.com>
2024-11-23 14:02:54 +0530

    Added the container for the exercises and a bottom navigation bar.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mentalhealth_app/utilities/emoticons.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({required this.name, super.key});
@@ -13,53 +14,202 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[700],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+        ],
+      ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
+          children: [
+            // Greetings Row
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Column(
                 children: [
-                  //Welcome
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text(
-                        'Hi ${widget.name}',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
+                      //Welcome
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hi ${widget.name}',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            '16 December 2024',
+                            style: TextStyle(
+                              color: Colors.blue[200],
+                            ),
+                          ),
+                        ],
+                      ),
+                      //Notifications
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        '16 December 2024',
-                        style: TextStyle(
-                          color: Colors.blue[200],
+                        padding: const EdgeInsets.all(15),
+                        child: const Icon(
+                          Icons.notifications,
+                          color: Colors.white,
                         ),
                       ),
                     ],
                   ),
-                  //Notifications
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  //Search bar
                   Container(
                     decoration: BoxDecoration(
                       color: Colors.blue[600],
                       borderRadius: BorderRadius.circular(10),
                     ),
                     padding: const EdgeInsets.all(15),
-                    child: const Icon(
-                      Icons.notifications,
-                      color: Colors.white,
+                    child: const Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
                     ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'How do you feel?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(
+                        Icons.more_horiz,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                  //Emoticons
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: Feeling.values
+                        .map((feeling) => Emoticons(feeling: feeling))
+                        .toList(),
                   ),
                 ],
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.grey[300],
+                child: Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Exercises',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                            Icon(Icons.more_horiz),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        //ListView
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          child: const Row(
+                            children: [
+                              Icon(Icons.favorite),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  //Title
+                                  Text(
+                                    'Speaking Skills',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 15,
+                                    ),
+                                  ),
+                                  //Subtitle
+                                  Text(
+                                    '15 more exercises',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
